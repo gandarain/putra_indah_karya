dcb2e838407ba776698665066f8785fd9436feb5
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2023-04-24 01:02:06 +0700

    Feature Content

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,6 +1,16 @@
 import React, { useEffect, useState, useCallback } from 'react';
 import { Container, Row, Col } from "reactstrap";
 
+import constants from "@/constants";
+
+const {
+  feature_constant: {
+    title,
+    subtitle,
+    content
+  }
+} = constants
+
 const FeatureBox = (props) => {
   return (
     <>
@@ -63,7 +73,6 @@ const useMediaQuery = (width) => {
     const media = window.matchMedia(`(max-width: ${width}px)`);
     media.addListener(updateTarget);
 
-    // Check on mount (callback is not called until a change occurs)
     if (media.matches) {
       setTargetReached(true);
     }
@@ -75,12 +84,6 @@ const useMediaQuery = (width) => {
 };
 
 const Feature = () => {
-  const features = [
-    {id : 1, img : "./images/45.png", title : "LOREM IPSUM", desc : "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", link : "/"},
-    {id : 2, img : "./images/46.png", title : "LOREM IPSUM", desc : "Sed perspiciatis unde omnis natus error voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo excepturi sint occaecati cupiditate architecto.", link : "/"},
-    {id : 3, img : "./images/45.png", title : "LOREM IPSUM", desc : "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.", link : "/"},
-  ];
-
   const isBreakpoint = useMediaQuery(768)
 
   return (
@@ -89,12 +92,12 @@ const Feature = () => {
         <Row className="justify-content-center">
           <Col lg={6} md={8}>
             <div className="title text-center mb-5">
-              <h3 className="font-weight-normal text-dark"><span className="text-warning">Features</span></h3>
-              <p className="text-muted">Lorem Ipsum has been the industryy&quot;s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book</p>
+              <h3 className="font-weight-normal text-dark"><span className="text-warning">{title}</span></h3>
+              <p className="text-muted">{subtitle}</p>
             </div>
           </Col>
         </Row>
-        <FeatureBox features={features} isBreakpoint={isBreakpoint} />
+        <FeatureBox features={content} isBreakpoint={isBreakpoint} />
       </Container>
     </section>
   );
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,13 @@
 import footer_content from "./footer_content"
 import hero_content from "./hero_content"
 import about_content from "./about_content"
+import feature_constant from "./feature_content"
 
 const constants = {
 	footer_content,
   hero_content,
-  about_content
+  about_content,
+  feature_constant
 }
   
 export default constants
