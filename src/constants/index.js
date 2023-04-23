b04a979096b81c77147ee93fcd5717ae3b4d9167
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2023-04-24 01:10:35 +0700

    Expertise Content

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,12 +2,14 @@ import footer_content from "./footer_content"
 import hero_content from "./hero_content"
 import about_content from "./about_content"
 import feature_constant from "./feature_content"
+import service_content from "./service_content"
 
 const constants = {
 	footer_content,
   hero_content,
   about_content,
-  feature_constant
+  feature_constant,
+  service_content
 }
   
 export default constants
---
+++ src/constants/service_content.js
@@ -0,0 +1,24 @@
+const title = "Expertise"
+const subtitle = "Jenis-jenis Keahlian perusahaan kami di berbagai bidang, Studi, Perencanaan dan Perancangan, Pengelolaan dan Pengawasan"
+const expertise = [
+  {
+    title : "Studi",
+    desc : "Studi Kelayakan, Studi Kebutuhan, Penyusunan Term Of Reference, Survei, Pengukuran dan Pemetaan, Survei Transportasi, Survei Pemasaran Studi Pengembangan Wilayah, Studi AMDAL (Analisa Mengenai Dampak Lingkungan)"
+  },
+  {
+    title : "Perencanaan dan Perancangan",
+    desc : "Bangunan dan Perumahan, Perkantoran (Gedung Kantor), Industri, pabrik, Hotel, Rumah Sakit/Klinik dan Permukiman. Jalan dan Jembatan serta Infrastruktur. Mekanikal dan Elektrikal. Penyediaan dan Ditribusi Air Bersih (Water Treatment Plan). Pelabuhan Laut dan Udara. Sipil (Bendungan, Jaringan Irigasi, dsbnya). Struktur dan Pondasi. Teknik Penyehatan dan Perpipaan. Sistem Telekomunikasi. Penyusunan Anggaran Biaya Proyek dan Estimasi Biaya (Value Engineering)"
+  },
+  {
+    title : "Pengelolaan dan Pengawasan",
+    desc : "Manajemen Proyek dan manajemen Konstruksi, Manajemen Keuangan, Pemasaran Logistik dan Sumber Daya manusia"
+  }
+]
+
+const service_content = {
+  title,
+  subtitle,
+  expertise
+}
+
+export default service_content
