8ac21ffc2075d595f36339422b27530596340522
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2023-04-25 16:23:12 +0700

    Enhance Eslint

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
-import Layout from '../components/Layout'
-import Header from '../components/Header'
-import Home from '../components/Home'
-import Services from '../components/Services'
-import Expertise from '../components/Expertise'
-import About from '../components/About'
-import Contact from '@/components/Contact'
-import Footer from '../components/Footer'
-import Project from '@/components/Project'
+import {
+  Layout,
+  Header,
+  Home,
+  About,
+  Services,
+  Expertise,
+  Project,
+  Contact,
+  Footer
+} from '../components'
 
 const Page = () => (
   <Layout pageTitle="Putra Indah Karya">
