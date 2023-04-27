a5124c6062762797a0c5b17b855ec2cef78654dc
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2023-04-27 21:12:22 +0700

    Change image services

--- src/constants/services_constant.js
+++ src/constants/services_constant.js
@@ -3,28 +3,28 @@ const subtitle = 'Jasa layanan konsultasi yang diberikan oleh CV. PUTRA INDAH KA
 const content = [
   {
     id : 1,
-    img : './images/study.png',
+    img : './images/services/1.jpg',
     title : 'Studi Kelayakan dan Amdal',
     desc : 'Studi kelayakan dihasilkan dengan cara menganalisa data berdasarkan rencana kegiatan yang telah disusun dengan mempertimbangkan proyeksi pengembangan yang mudah dicapai. Keakuratan dan ketajaman analisis yang dapat memberikan gambaran jelas didalam menentukan kelanjutan suatu rancangan kegiatan pembangunan. Studi kelayakan seyogyanya dilengkapi dengan Amdal sehingga dapat diamati dampak yang timbul, untuk itu kemudian diajukan beberapa alternatif pemecahaannya.',
     ids : 'appropriateness'
   },
   {
     id : 2, 
-    img : './images/management.png', 
+    img : './images/services/2.jpg',
     title : 'Manajemen Proyek dan manajemen Konstruksi', 
     desc : 'Kegiatan manajemen Proyek dan manajemen Konstruksi meliputi atas 3 (tiga) tahap yang terpenting antara lain : Tahap Persiapan, Tahap Perancangan, Tahapan Pelaksanaan',
     ids : 'construction'
   },
   {
     id : 3, 
-    img : './images/development.png', 
+    img : './images/services/3.jpg',
     title : 'Studi Pengembangan Wilayah', 
     desc : 'Untuk meletakkan dasar-dasar pembangunan dan pengembagan wilayah suatu daerah tentunya sangat diperlukan suatu bentuk studi yang akurat dan teliti agar daerah tersebut dapat tumbuh berkembang sesuai dengan kemajuan pengembangan wilayah itu sendiri serta tidak terlepas dari ketentuan-ketentuan maupun kebijakan yang telah ditetapkan oleh Pemerintah. Pembuatan Rencana Umum Tata Ruang (RUTR), Rencana Detail Tata Ruang (RDTR), Rencana Tata Bangunan dan Lingkungan (RTBL) maupun yang lainnya memerlukan suatu studi yang komprehensif agar Pemerintah, khususnya Pemerintah Daerah dapat membuat Peraturan Daerah (Perda) yang akan menjadi pedoman pengembangan wilayah itu sendiri.',
     ids : 'development'
   },
   {
     id : 4,
-    img : './images/measurement.png', 
+    img : './images/services/4.jpg',
     title : 'Survey Pengukuran/Topografi', 
     desc : 'Di dalam realisasi pembangunan, pola perencanaan adalah sangat menentukan agar pelaksanaan pembangunan/proyek tersebut dapat sesuai dengan ketersediaan biaya, waktu serta sasaran kualitas. Di dalam perencanaan, data awal yang sangat dibutuhkan adalah existing kondisi lokasi yang akan direkayasa/dibangun agar menjadi suatu konsep bangunan yang sesuai dengan keinginan dan tujuan dari si pengguna jasa. Khusus untuk perencanaan jaringan irigasi, pola perencanaan akan sangat ditentukan oleh hasil pengukuran topografi dari lokasi yang akan direncanakan. Kesalahan pengukuran dan pemetaan situasi akan sangat berdampak kepada pola perencanaan serta realisasi pembangunannya. Demikian pula halnya untuk perencanaan suatu bangunan, dasar dari pengukuran akan sangat mempengaruhi pola perencanaan, antara lain penentuan tapak bangunan dan peil lantai bangunan serta siteplan bangunan.',
     ids : 'measurement'
