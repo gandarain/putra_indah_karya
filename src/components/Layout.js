c3023df903d0cf5d795d54e2be7a8d0f7ea4af45
Ganda Rain Panjaitan <gandarainpanjaitan@gmail.com>
2023-05-02 16:59:51 +0700

    Verify Ownership

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,6 +9,7 @@ const Layout = (props) => {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <meta name="google-site-verification" content={process.env.GOOGLE_SITE_VERIFICATION} />
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
